e0ee236a2e69d055533de31ba20c4235d801adb0
sjyjytu <1332372004@qq.com>
2019-07-30 11:16:37 +0800

    init

---
+++ src/main/java/com/mosaiker/recordservice/RecordserviceApplication.java
@@ -0,0 +1,13 @@
+package com.mosaiker.recordservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecordserviceApplication.class, args);
+    }
+
+}
