91d0a9856b762ba7b7ef8ca6f2e243321b4157b4
kty <1332372004@qq.com>
2019-08-26 17:08:05 +0800

    fix: fix message list size

--- src/main/java/com/mosaiker/recordservice/controller/MessageController.java
+++ src/main/java/com/mosaiker/recordservice/controller/MessageController.java
@@ -35,7 +35,7 @@ public class MessageController {
         JSONArray list = new JSONArray();
         for (int type = 0; type < 5; type++) {
             List<Message> messages = messageService.findMessagesByReceiverUIdAndType(uId, type);
-            if (messages == null) {
+            if (messages == null || messages.size() == 0) {
                 continue;
             }
             JSONObject tmp = messages.get(messages.size() - 1).ToJSONObject();
