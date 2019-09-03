c2160d5034259527e82d39ae0048113193a536c0
kty <1332372004@qq.com>
2019-09-03 12:17:27 +0800

    feat: add generate poem

--- src/main/java/com/mosaiker/recordservice/controller/MoodReportController.java
+++ src/main/java/com/mosaiker/recordservice/controller/MoodReportController.java
@@ -2,7 +2,10 @@ package com.mosaiker.recordservice.controller;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.mosaiker.recordservice.service.HttpService;
 import com.mosaiker.recordservice.service.MoodReportService;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -47,6 +50,22 @@ public class MoodReportController {
     //然后新增moodReport，这周没发过函的不给他生成心情报表
     JSONObject ret = new JSONObject();
     ret.put("rescode", 0);
+    return ret;
+  }
+
+  @RequestMapping(value = "/moodReport/generatePoem", method = RequestMethod.POST)
+  @ResponseBody
+  public JSONObject generatePoem() throws UnsupportedEncodingException {
+    JSONObject ret = new JSONObject();
+    String result = HttpService.doPost("http://10.0.0.98:11370/predict", "");
+    JSONObject parseObject = JSONObject.parseObject(result);
+    if (parseObject.getBoolean("success")) {
+      ret.put("rescode", 0);
+      ret.put("originPoem", parseObject.getString("poem"));
+      ret.put("poem", URLDecoder.decode(parseObject.getString("poem"), "UTF-8"));
+    } else {
+      ret.put("rescode", 1);
+    }
     return ret;
   }
 
---
+++ src/test/java/com/mosaiker/recordservice/service/HttpServiceTest.java
@@ -0,0 +1,24 @@
+package com.mosaiker.recordservice.service;
+
+import static org.junit.Assert.*;
+
+import com.alibaba.fastjson.JSONObject;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import org.junit.Test;
+
+public class HttpServiceTest {
+
+  @Test
+  public void doGet() {
+  }
+
+  @Test
+  public void doPost() throws UnsupportedEncodingException {
+    String result = HttpService.doPost("http://127.0.0.1:11370/predict", "");
+    JSONObject parseObject = JSONObject.parseObject(result);
+    System.out.println(result);
+    String strUTF8 = URLDecoder.decode(parseObject.getString("poem"), "UTF-8");
+    System.out.println(strUTF8);
+  }
+}
