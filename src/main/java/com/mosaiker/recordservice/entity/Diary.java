6b96a03e7c274bad20c2471d21ef0f21bddc8824
kty <1332372004@qq.com>
2019-09-07 20:00:30 +0800

    fix: fix diary

--- src/main/java/com/mosaiker/recordservice/entity/Diary.java
+++ src/main/java/com/mosaiker/recordservice/entity/Diary.java
@@ -5,11 +5,14 @@ import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "diary")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
